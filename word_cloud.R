f2da469131d7ee2135a8bc1d526be85e5f0b295a
Angelita Bastos Regis Guedes <71788259+angelitabrg@users.noreply.github.com>
2023-10-02 14:38:20 -0300

    Create word_cloud.R
    libraries

---
+++ word_cloud.R
@@ -0,0 +1,10 @@
+# importing libraries
+library("dplyr")
+library("tidytext")
+library("ggplot2")
+library("lexiconPT")
+library("janeaustenr")
+library("stringr")
+library("tidyr")
+library("wordcloud")
+
